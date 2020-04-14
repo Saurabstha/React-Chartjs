4fa95f68dbc86868e73fd57cf11d9edc4e52e7b6
unknown <saurab.shrestha99@gmail.com>
2020-04-14 18:26:30 +0545

    router and grid

--- src/App.js
+++ src/App.js
@@ -1,33 +1,38 @@
 import React from 'react';
 import './App.css';
-import * as ReactBootStrap from "react-bootstrap";
+
+import Navbar from './components/Navbar';
+import DankMemes from './components/Dankmemes';
+import Features from './components/Features';
+import Pricing from './components/Pricing';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <ReactBootStrap.Navbar collapseOnSelect expand="sm" bg="dark" variant="dark">
-        <ReactBootStrap.Navbar.Brand href="#home">React-Bootstrap</ReactBootStrap.Navbar.Brand>
-        <ReactBootStrap.Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <ReactBootStrap.Navbar.Collapse id="responsive-navbar-nav">
-          <ReactBootStrap.Nav className="mr-auto">
-            <ReactBootStrap.Nav.Link href="#features">Features</ReactBootStrap.Nav.Link>
-            <ReactBootStrap.Nav.Link href="#pricing">Pricing</ReactBootStrap.Nav.Link>
-            <ReactBootStrap.NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-              <ReactBootStrap.NavDropdown.Item href="#action/3.1">Action</ReactBootStrap.NavDropdown.Item>
-              <ReactBootStrap.NavDropdown.Item href="#action/3.2">Another action</ReactBootStrap.NavDropdown.Item>
-              <ReactBootStrap.NavDropdown.Item href="#action/3.3">Something</ReactBootStrap.NavDropdown.Item>
-              <ReactBootStrap.NavDropdown.Divider />
-              <ReactBootStrap.NavDropdown.Item href="#action/3.4">Separated link</ReactBootStrap.NavDropdown.Item>
-            </ReactBootStrap.NavDropdown>
-          </ReactBootStrap.Nav>
-          <ReactBootStrap.Nav>
-            <ReactBootStrap.Nav.Link href="#deets">More deets</ReactBootStrap.Nav.Link>
-            <ReactBootStrap.Nav.Link eventKey={2} href="#memes">
-              Dank memes
-            </ReactBootStrap.Nav.Link>
-          </ReactBootStrap.Nav>
-        </ReactBootStrap.Navbar.Collapse>
-      </ReactBootStrap.Navbar>
+      
+    <Router>
+      <Navbar />
+        
+
+      <Switch>
+        <Route path="/features" component={Features}>
+          <Features />
+        </Route>
+        <Route path="/pricing" component={Pricing}>
+          <Pricing />
+        </Route>
+        <Route path="/dankMemes" component={DankMemes}>
+          <DankMemes />
+        </Route>
+      </Switch>
+    </Router>
     </div>
   );
 }
---
+++ src/components/Dankmemes.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Dankmemes = () => {
+    return (
+        <div>
+            <h1>These are Danke memes</h1>
+        </div>
+    )
+}
+
+export default Dankmemes;
